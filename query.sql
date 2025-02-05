d32413322185c798e91a72f1e431e39830d4c71f
HP_Nam <namphg1412@gmail.com>
2025-02-05 08:06:04 +0700

    bai 1

---
+++ query.sql
@@ -0,0 +1,13 @@
+DROP TABLE sinh_vien
+
+CREATE TABLE sinh_vien(
+	ten_sinh_vien nvarchar(50),
+  	tuoi int,
+  	gioi_tinh bit,
+  	so_dien_thoai char(15)
+)
+
+INSERT INTO sinh_vien(ten_sinh_vien, tuoi, gioi_tinh, so_dien_thoai)
+VALUES (N'Tuấn', 20, 1, '0123456789')
+
+SELECT * FROM sinh_vien
